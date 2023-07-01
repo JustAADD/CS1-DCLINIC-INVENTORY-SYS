7ed4aec36775e7364bc5de4a29f472d32dad6569
Adrian Rodrigo Makiling <makilingadrian19@gmail.com>
2023-07-01 15:18:46 +0800

    first commit

--- header.php
+++ header.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <!-- ===== Bootstrap CSS ===== -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <!-- ===== CSS ===== -->
   <link rel="stylesheet" href="assets/css/style.css">
 
@@ -18,12 +21,12 @@
     <ion-icon name="menu-outline" class="header__toggle" id="nav-toggle"></ion-icon>
 
     <nav class="nav" id="nav-menu">
-      <div class="nav__content bd-grid">
+      <div class="bd-grid" id="nav__content">
         <ion-icon name="close-outline" class="nav__close" id="nav-close"></ion-icon>
 
         <div class="nav__perfil">
           <div class="nav__img">
-            <img src="assets/img/perfil.png" alt="">
+            <img src="assets/image/dalino_logo.png" alt="">
           </div>
 
           <div>
@@ -34,7 +37,7 @@
         <div class="nav__menu">
           <ul class="nav__list">
             <li class="nav__item"><a href="#" class="nav__link active">Home</a></li>
-            <li class="nav__item"><a href="#" class="nav__link">Service</a></li>
+            <li class="nav__item"><a href="#" class="nav__link">Services</a></li>
             <li class="nav__item"><a href="#" class="nav__link">Appointment</a></li>
             <li class="nav__item"><a href="#" class="nav__link">About us</a></li>
 
@@ -42,19 +45,26 @@
         </div>
 
         <div class="nav__social">
+          <a href="#" class="nav__social-icon"><ion-icon name="mail-outline"></ion-icon></a>
+          <a href="#" class="nav__social-icon"><ion-icon name="logo-facebook"></ion-icon></a>
           <a href="#" class="nav__social-icon"> <ion-icon name="log-out-outline"></ion-icon></a>
-          <!--<a href="#" class="nav__social-icon"><ion-icon name="logo-github"></ion-icon></a>
-          <a href="#" class="nav__social-icon"><ion-icon name="logo-behance"></ion-icon></a>-->
         </div>
       </div>
 
     </nav>
 
   </header>
+  
+
 
   <!-- ===== IONICONS ===== -->
   <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
 
+  <!--===== Bootstrap JS =====-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+    
   <!--===== MAIN JS =====-->
   <script src="assets/js/main.js"></script>
 </body>
---
+++ home.php
@@ -0,0 +1,99 @@
+<?php
+
+require_once 'header.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- ===== FIRST CONTENT ===== -->
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- ===== css ===== -->
+  <link rel="stylesheet" href="assets/css/content-style.css">
+
+</head>
+
+<body>
+
+  <div class="sec" style="margin-top: 5rem"></div>
+
+  <!-- first column and row -->
+
+  <!-- <section id="hero" class="hero">
+    <div class="container-fluid position-relative">
+      <div class="row gy-5">
+        <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center text-center text-lg-start">
+          <h2>We Care About <br> Your <span>Smile</span></h2>
+          <p>We will take care of your dental health. <br>
+            Choose your desired time below and <br>
+            we'll help out. <br>
+          </p>
+          <div class="d-flex justify-content-center justify-content-lg-start mt-4">
+            <a href="#about" class="btn-get-started me-3">Get in Touch</a>
+            <a href="#about" class="btn-get-appointment">Make an Appointment</a>
+          </div>
+        </div>  -->
+
+  <!-- ===== second col ===== -->
+  <!-- <div class="col-lg-6 order-1 order-lg-1" id="coltwo">
+          <img src="">
+        </div>
+      </div>
+  </section>  -->
+
+
+  <!-- second column and row -->
+
+  <section class="cons" id="cons">
+    <div class="row m-0">
+      <div class="col-sm-6" id="columnOne">
+        <h2>We Care About <br> Your <span>Smile</span></h2>
+        <p>We will take care of your dental health. <br>
+          Choose your desired time below and <br>
+          we'll help out. <br>
+        </p>
+        <div class="d-flex justify-content-center justify-content-lg-start mt-4" id="boton">
+          <a href="#about" class="btn-get-started me-3">Get in Touch</a>
+          <a href="#about" class="btn-get-appointment">Make an Appointment</a>
+        </div>
+
+        <!-- Your Appointment Schedule -->
+
+        <div class="App-sched mt-5">
+          <div class="card" style="width: 25rem;">
+            <div class="card-body">
+              <h5>Your Appointment Schedule:</h5>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="col-sm-6 p-0">
+        <div class="rectangle">
+          <div class="columntwo">
+            <p> Our dental health services prioritize your oral well-being. <br>
+              Trust your oral well-being. Trust our Experienced services to <br>
+              Guide you on your journey to optimal dental health.
+            </p>
+
+            <div class="model">
+              <img src="assets/image/model.svg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </section>
+
+
+
+</body>
+
+</html>
